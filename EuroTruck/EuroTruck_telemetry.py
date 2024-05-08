144c7888cdef6d08733d26a279f86012fbeefdd4
Ruoqing <s262306@studenti.polito.it>
2024-05-08 11:01:18 +0200

    async

--- EuroTruck/EuroTruck_telemetry.py
+++ EuroTruck/EuroTruck_telemetry.py
@@ -1,10 +1,21 @@
 import json
 import requests
 import csv
-import sseclient
+import aiohttp
+import asyncio
 
 
-def process_data(data):
+async def get_data(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                print("Failed to fetch data from the URL.")
+                return None
+
+
+async def write_data(data):
     if data["game"]["connected"]:
         print("Game connected")
         with open("telemetry.csv", "a", newline='') as f:
@@ -21,11 +32,18 @@ def process_data(data):
         print("Error: Game not connected")
 
 
-if __name__ == '__main__':
-    url = "http://localhost:25555/api/ets2/telemetry"  # not public internet, please connect to local server
-    response = requests.get(url)
-    client = sseclient.SSEClient(response)
+async def job(url):
+    game_data = await get_data(url)
+    await write_data(game_data)
 
+
+async def main(url):
+    while True:
+        await job(url)
+        await asyncio.sleep(0.01)
+
+if __name__ == '__main__':
+    url = "http://localhost:25555/api/ets2/telemetry"
     with open("telemetry.csv", "w", newline='') as f:
         writer = csv.writer(f)
         writer.writerow(
@@ -33,9 +51,8 @@ if __name__ == '__main__':
              "userBrake",
              "gameSteer", "placement_x", "placement_y", "acceleration_x", "acceleration_y"])
 
-    for event in client:
-        data = json.loads(event.data)
-        process_data(data)
+    asyncio.run(main(url))
+
 
 
 
